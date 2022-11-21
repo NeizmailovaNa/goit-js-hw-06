d72077dc84788bed6973a8695b1433af00f32ce7
Neizmailova Natalia <natata.neizmailova@gmail.com>
2022-11-21 12:18:09 +0200

    Task-1.1-1.2 js

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,21 @@
 const numberOfCategories = document.querySelectorAll('.item');
 console.log('Number of categories:', numberOfCategories.length);
 
+const nameOfFirstCategory = document.querySelector('h2');
+console.log('Category:', nameOfFirstCategory.textContent);
+
+const numberOfElementsOfFirstCategory = document.querySelector('.item').querySelectorAll('li');
+console.log('Elements:', numberOfElementsOfFirstCategory.length);
+
+/*const nameOfSecondCategory = document.querySelector('h2');
+console.log('Category:', nameOfSecondCategory.textContent);
+
+const numberOfElementsOfSecondCategory = document.querySelector('.item').querySelectorAll('li');
+console.log('Elements:', numberOfElementsOfSecondCategory.length);
+
+const nameOfThirdCategory = document.querySelector('h2');
+console.log('Category:', nameOfThirdCategory.textContent);
+
+const numberOfElementsOfThirdCategory = document.querySelector('.item').querySelectorAll('li');
+console.log('Elements:', numberOfElementsOfThirdCategory.length);
+*/
