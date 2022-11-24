ede93cf8624146d3083a0cbecc6f17cb5e3033e4
Neizmailova Natalia <natata.neizmailova@gmail.com>
2022-11-24 14:51:57 +0200

    task-04.js-finish

--- js/task-04.js
+++ js/task-04.js
@@ -1,12 +1,16 @@
+const minusBtn = document.querySelector('button');
+const plusBtn = document.querySelector('div').lastElementChild;
+const valueEl = document.querySelector('#value');
+
 const counter = {
     value: 0,
     increment (value) {
-        console.log(`increment -> this`, this);
         this.value += value
+        valueEl.textContent = this.value
     },
     decrement (value) {
-        console.log(`increment -> this`, this);
         this.value -= value
+        valueEl.textContent = this.value
     },
 };
 
@@ -14,15 +18,11 @@ const counterValue = function (value, operation) {
     operation(value)
 };
 
-const minusBtn = document.querySelector('button');
-const plusBtn = document.querySelector('div').lastElementChild;
-const valueEl = document.querySelector('#value');
-
 plusBtn.addEventListener ('click', () => {
-    console.log(counterValue(1, counter.increment.bind(counter)));
+    counterValue(1, counter.increment.bind(counter));
 });
 
 minusBtn.addEventListener ('click', () => {
-    console.log(counterValue(1, counter.decrement.bind(counter)));
+    counterValue(1, counter.decrement.bind(counter));
 });
 
